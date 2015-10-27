62c1d6f28541e8105fa5fd427628d2f9ff623a0c
pmontu <pmontu@gmail.com>
2015-10-27 21:51:38 +0530

    second

--- user_story.py
+++ user_story.py
@@ -1,19 +1,13 @@
 input_file = open("input01.txt","r")
 
-account_number_pipes_underscores = []
-account_number_pipes_underscores.append(input_file.readline().rstrip())
-account_number_pipes_underscores.append(input_file.readline().rstrip())
-account_number_pipes_underscores.append(input_file.readline().rstrip())
-for line in account_number_pipes_underscores: print line
+entries = []
 
-s = ""
-for i in range(1,10):
-	 s += str(i)
-s += s + s
-print s
+for line in input_file:
+	entries.append(line.rstrip())
+
+count = (len(entries)+1)/4
 
 i = 1
 for j in range(9):
 	print i, i+1, i+2
 	i+=3
-	
