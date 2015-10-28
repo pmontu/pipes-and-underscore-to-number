52ad8aef5141a79e4757a54e50f1acab7b8b5fa2
Manoj Kumar P <pmontu@gmail.com>
2015-10-28 15:30:05 +0530

    permutations of a digit

---
+++ permute.py
@@ -0,0 +1,15 @@
+i=0
+i0 = " "
+for i1 in [" ","_"]:
+	i2 = " "
+	for i3 in  [" ","|"]:
+		for i4 in  [" ","_"]:
+			for i5 in  [" ","|"]:
+				for i6 in  [" ","|"]:
+					for i7 in  [" ","_"]:
+						for i8 in  [" ","|"]:
+							print (i)
+							print ("{0}{1}{2}".format(i0,i1,i2))
+							print ("{0}{1}{2}".format(i3,i4,i5))
+							print ("{0}{1}{2}".format(i6,i7,i8))
+							i+=1
--- user_story_123.py
+++ user_story_123.py
@@ -1,6 +1,7 @@
 config = [
 	{"input":"input01.txt", "output":"output01.txt", "level":1},
 	{"input":"input02.txt", "output":"output02.txt", "level":3},
+	{"input":"input03.txt", "output":"output03.txt", "level":4},
 ]
 CONFIG_NUM = 1
 input_file = open(config[CONFIG_NUM]["input"],"r")
@@ -11,12 +12,14 @@ for line in input_file:
 	entries.append(line.rstrip("\n"))
 
 #number of entries in input file
-count = (len(entries)+1)/4
+count = int((len(entries)+1)/4)
 acc_nums = []
+#vertical traversal
 for z in range(count):
 	#column start of every digit
 	i = 0
 	digits = []
+	#horizontal traversal
 	for j in range(9):
 		#3 lines height, 9 digits each 3 columns width
 		digit = []
@@ -28,6 +31,7 @@ for z in range(count):
 		digits.append(digit)
 	acc_nums.append(digits)
 
+#all digits respective pipe and underscore value. 3 rows and 3 columns expressed in 1 row and 9 columns.
 master = [
 	{"string":" _ | ||_|","number":0},
 	{"string":"     |  |","number":1},
@@ -66,13 +70,26 @@ for num in acc_nums:
 			s += line
 		account_num += str(search(master, s))
 
+#check ambivalence
+for num in acc_nums:
+	account_num = ""
+	
+	for digit in num:
+		s = ""
+		for line in digit:
+			s += line
+		account_num += str(search(master, s))
 	expected = output_file.readline().rstrip("\n")
 	if config[CONFIG_NUM]["level"]==1:
 		output = account_num
 		assert output == expected
-	if config[CONFIG_NUM]["level"]>=3:
+		print (output)
+	if config[CONFIG_NUM]["level"]==3:
 		output = account_num
+		#analysis of check sum and illegible digits
 		check_val = check(account_num)
 		if check_val:
 			output += check_val.rjust(4, " ")
 		assert output == expected
+		print (output)
+	if config[CONFIG_NUM]["level"]==4:
