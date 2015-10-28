553d8ddac48582bd59b699611ebccef2702fb639
pmontu <pmontu@gmail.com>
2015-10-28 10:47:28 +0530

    user stories till 3

--- user_story.py
+++ user_story.py
@@ -1,21 +1,78 @@
-input_file = open("input01.txt","r")
+config = [
+	{"input":"input01.txt", "output":"output01.txt", "level":1},
+	{"input":"input02.txt", "output":"output02.txt", "level":3},
+]
+CONFIG_NUM = 1
+input_file = open(config[CONFIG_NUM]["input"],"r")
 
 entries = []
 
 for line in input_file:
-	entries.append(line.rstrip())
+	entries.append(line.rstrip("\n"))
 
+#number of entries in input file
 count = (len(entries)+1)/4
+acc_nums = []
 for z in range(count):
+	#column start of every digit
 	i = 0
 	digits = []
 	for j in range(9):
+		#3 lines height, 9 digits each 3 columns width
 		digit = []
-		digit.append(entries[z*4][i:i+3])
+		digit.append(entries[z*4][i:i+3].ljust(3," "))
 		digit.append(entries[z*4+1][i:i+3])
 		digit.append(entries[z*4+2][i:i+3])
+		#ignore 4th line
 		i+=3
 		digits.append(digit)
-for digit in digits:
-	for line in digit:
-		print line
+	acc_nums.append(digits)
+
+master = [
+	{"string":" _ | ||_|","number":0},
+	{"string":"     |  |","number":1},
+	{"string":" _  _||_ ","number":2},
+	{"string":" _  _| _|","number":3},
+	{"string":"   |_|  |","number":4},
+	{"string":" _ |_  _|","number":5},
+	{"string":" _ |_ |_|","number":6},
+	{"string":" _   |  |","number":7},
+	{"string":" _ |_||_|","number":8},
+	{"string":" _ |_| _|","number":9},
+]
+
+def search(master, s):
+	d = "?"
+	for item in master:
+		if s == item["string"]:
+			return item["number"]
+	return d
+
+def check(account_num):
+	if account_num.find("?") == -1:
+		check_sum = 0
+		for index, digit in enumerate(account_num):
+			check_sum += (9-index) * int(digit)
+		if check_sum % 11 == 0 : return ""
+		else: return "ERR"
+	else: return "ILL"
+
+output_file = open(config[CONFIG_NUM]["output"],"r")
+for num in acc_nums:
+	account_num = ""
+	for digit in num:
+		s = ""
+		for line in digit:
+			s += line
+		account_num += str(search(master, s))
+
+	expected = output_file.readline().rstrip("\n")
+	if config[CONFIG_NUM]["level"]==1:
+		output = account_num
+		assert output == expected
+	if config[CONFIG_NUM]["level"]>=3:
+		output = account_num
+		check_val = check(account_num)
+		if check_val:
+			output += check_val.rjust(4, " ")
+		assert output == expected
