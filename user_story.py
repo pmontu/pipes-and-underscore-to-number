988ec033acbbf6d8c2b9ee74942b4b9fd1a73d08
pmontu <pmontu@gmail.com>
2015-10-28 08:06:52 +0530

    extracted digits

--- user_story.py
+++ user_story.py
@@ -6,8 +6,16 @@ for line in input_file:
 	entries.append(line.rstrip())
 
 count = (len(entries)+1)/4
-
-i = 1
-for j in range(9):
-	print i, i+1, i+2
-	i+=3
+for z in range(count):
+	i = 0
+	digits = []
+	for j in range(9):
+		digit = []
+		digit.append(entries[z*4][i:i+3])
+		digit.append(entries[z*4+1][i:i+3])
+		digit.append(entries[z*4+2][i:i+3])
+		i+=3
+		digits.append(digit)
+for digit in digits:
+	for line in digit:
+		print line
